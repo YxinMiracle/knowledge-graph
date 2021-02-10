c2df6cd75f7a63a8dd9add1ff1d570b878cc5c77
939157765@qq.com <939157765@qq.com>
2021-02-10 09:48:39 +0800

    nfu-knowledge-graph

--- QuestionSpider/QuestionSpider/pipelines.py
+++ QuestionSpider/QuestionSpider/pipelines.py
@@ -7,8 +7,41 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from twisted.enterprise import adbapi  # 实现异步入库
 
 class QuestionspiderPipeline:
     def process_item(self, item, spider):
         print(item)
         return item
+
+class MysqlTwistedPipeline(object):  # 异步入库
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls, settings):
+        from MySQLdb.cursors import DictCursor
+        dbparms = dict(
+            host=settings["MYSQL_HOST"],
+            db=settings["MYSQL_DBNAME"],
+            user=settings["MYSQL_USER"],
+            passwd=settings["MYSQL_PASSWORD"],
+            charset="utf8",
+            cursorclass=DictCursor,
+            use_unicode=True,
+        )
+        dbpool = adbapi.ConnectionPool("MySQLdb", **dbparms)
+        return cls(dbpool)
+
+    def process_item(self, item, spider):
+        query = self.dbpool.runInteraction(self.do_insert, item)
+        query.addErrback(self.handle_error, item, spider)
+
+    def handle_error(self, failure, item, spider):
+        print(failure)
+
+    def do_insert(self, cursor, item):
+        # 根据不同的ITEM 构建不同的sql语句并插入到mysql中
+        insert_sql, params = item.get_insert_sql()
+        cursor.execute(insert_sql, tuple(params))
+        print("成功插入：", params)
